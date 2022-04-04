19df9597ae54eded6b2e70b4de32c7645984188f
M Atif <chatif476@gmail.com>
2022-04-04 07:35:30 +0500

    part3

--- Part3.py
+++ Part3.py
@@ -1,85 +1,120 @@
-# The default values
+import csv
+
+
 print("Student : M Atif")
 print("Student id : 18B-075-SE(A)")
-# A starting point for the entries
-endFlag = False
-# lists to store the entries
-list = []
-record = []
-# loop to get the entries
-while endFlag == False:
-    # valid entry checker
-    checker = True
-    TT = 0
-    name = input("Diver's Name: ")
-    dificulty = float(input("Diver's Dificulty(1-5): "))
-    # check if the dificulty is valid
-    if dificulty > 5 or dificulty < 1:
-        print("Invalid Dificulty")
-        checker = False
-    else:
-        pass
 
-    if name == '':
-        checker = False
-        # check if the name is valid
-    if checker == True:
-        # append the entries to the list
-        emptyList = []
-        tempList = []
-        emptyList.append(name)
 
-        judge = 1
-        # loop to get the judges
-        while judge != 6:
-            # valid entry checker
-            if checker == True:
-                x = input(f"Judge {judge} : ")
-                # blank entry checker
-                if x == '':
-                    print("Blank Entry")
-                    checker = False
+rounds = []
+menu = """
+1. Enter Round Number
+2. Enter Diver Data
+3. Generate Report
+4. Create csv file
+5. Quit
+"""
+while True:
+    print(menu)
+    userInput = input("Select an option: ")
+    if userInput == '5':
+        break
+    # There are multiple rounds where each diver competes. Allow the user to set the Round Number
 
-                # check if the entry is valid
-                elif float(x) >= 0 and float(x) <= 10:
-                    emptyList.append(x)
-                    tempList.append(x)
+    elif userInput == '1':
+        roundNumber = int(input("Enter Round Number: "))
+        rounds.append(roundNumber)
+        print("Round Number: ", roundNumber)
 
-                else:
-                    print("Invalid Entry")
-                    checker = False
-                # increment the judge
-                judge += 1
-
+    elif userInput == '2':
+        checker = True
+        endFlag = False
+        List = []
+        judgeScore = []
+        dificultyBigStore = []
+        record = []
+        allRecord = []
+        while endFlag == False:
+            TT = 0
+            dificultyStore = []
+            name = input("Diver's Name: ")
+            dificulty = float(input("Diver's Dificulty(1-5): "))
+            if dificulty > 5 or dificulty < 1:
+                print("Invalid Dificulty")
+                checker = False
             else:
-                break
-        # sorted the list and append it to the record
-        tempList = sorted(tempList)
-        tempList = tempList[1:len(tempList) - 1]
-        smallRecord = []
-        for i in tempList:
-            TT += float(i)
-        # store TT and Final in list
-        smallRecord.append(TT)
-        smallRecord.append(round(TT * dificulty, 2))
-        record.append(smallRecord)
-        # append the list to the list
-        if checker == True:
-            list.append(emptyList)
-    # check if the user wants to end the program
-    userInput = input("Continue? [Y/N] ")
-    # continue until the condition is met
-    while userInput.lower() != 'y' and userInput.lower() != 'n':
-        userInput = input("Continue? [Y/N] ")
+                dificultyStore.append(dificulty)
+                pass
 
-    if userInput.lower() == 'n':
-        endFlag = True
+            if name == '':
+                checker = False
+            if checker == True:
+                emptyList = []
+                tempList = []
+                tempJudgeScore = []
+                emptyList.append(name)
 
-# Showing the final result
+                judge = 1
+                while judge != 6:
+                    if checker == True:
+                        x = input(f"Judge {judge} : ")
+                        if x == '':
+                            print("Blank Entry")
+                            checker = False
 
-for item in list:
-    for items in item:
-        print(items, end='\t')
-    print(
-        f"TT: {record[list.index(item)][0]}\tScore: {record[list.index(item)][1]}")
-    print()
+                        elif float(x) >= 0 and float(x) <= 10:
+                            emptyList.append(float(x))
+                            tempList.append(float(x))
+                            tempJudgeScore.append(float(x))
+
+                        else:
+                            print("Invalid Entry")
+                            checker = False
+                        judge += 1
+                    else:
+                        break
+                dificultyBigStore.append(dificultyStore)
+                # append the round number to the List
+                judgeScore.append(tempJudgeScore)
+                tempList = sorted(tempList)
+                tempList = tempList[1:len(tempList) - 1]
+                smallRecord = []
+                for i in tempList:
+                    TT += float(i)
+                smallRecord.append(TT)
+                smallRecord.append(round(TT * dificulty, 2))
+                record.append(smallRecord)
+                if checker == True:
+                    List.append(emptyList)
+
+            userInput = input("Continue? [Y/N] ")
+            while userInput.lower() != 'y' and userInput.lower() != 'n':
+                userInput = input("Continue? [Y/N] ")
+
+            if userInput.lower() == 'n':
+                endFlag = True
+
+            # print all the data in format of Name,difficulty,Judges Scores,total time,total score
+                # print("\n")
+                # for i in range(len(List)):
+                #     print(
+                #         f"Name :{List[i][0]}\t DD: {dificultyBigStore[i]}\t Scores : {judgeScore[i]}\t TT : {record[i][0]}\t Final Score: {record[i][1]}")
+                # print("\n")
+
+    elif userInput == '3':
+        # generate the report of all divers with round number
+        print("\n")
+        for i in range(len(List)):
+            nameData = List[i][0]
+            print(
+                f"Name :{nameData}\t DD: {dificultyBigStore[i]}\t Scores : {judgeScore[i]}\t TT : {record[i][0]}\t Final Score: {record[i][1]}")
+        print("\n")
+
+    elif userInput == '4':
+        # export all the data to csv file while new data can be appended but the header remain same
+
+        with open('report.csv', 'a', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=[
+                                    'Name', 'Difficulty', 'Judges Scores', 'Total Time', 'Final Score'])
+            for i in range(len(List)):
+                writer.writerow({"Name": List[i][0], "Difficulty": dificultyBigStore[i],
+                                "Judges Scores": judgeScore[i], "Total Time": record[i][0], "Final Score": record[i][1]})
